27d91965d4dfab49934e83657d7dabce6643ea61
Artem Chirkin <achirkin@nvidia.com>
2022-05-02 01:38:39 -0700

    restore gpu name in the reports

--- run.sh
+++ run.sh
@@ -2,9 +2,11 @@
 export NVIDIA_VISIBLE_DEVICES=0
 export CUDA_VISIBLE_DEVICES=0
 
-MEM_CLOCK=$(nvidia-smi -i 0 --query-gpu=clocks.applications.memory --format=csv,noheader,nounits)
-GPU_CLOCK=$(nvidia-smi -i 0 --query-gpu=clocks.applications.graphics --format=csv,noheader,nounits)
-REPORT_NAME=knn_$(date +"%Y.%m.%d-%H.%M")_${MEM_CLOCK}-${GPU_CLOCK}.csv
+GPU_SPEC=$(nvidia-smi -i 0 --query-gpu=name,clocks.applications.memory,clocks.applications.graphics --format=csv,noheader,nounits)
+GPU_SPEC=${GPU_SPEC//, /-}
+GPU_SPEC=${GPU_SPEC//,/-}
+GPU_SPEC=${GPU_SPEC// /-}
+REPORT_NAME=knn_$(date +"%Y.%m.%d-%H.%M")_${GPU_SPEC}.csv
 
 ./cpp/build/bench_raft \
     --benchmark_filter=KNN/.*/manual_time \
